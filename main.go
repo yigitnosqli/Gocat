f83961b9173741c29e36e39bf23fb35e2eb45c36
yiğit ibrahim <ibrahimsql@proton.me>
2025-10-04 22:13:27 +0300

    feat: Add showVersion() function back to main.go
    - Displays detailed build information
    - Shows runtime information (Go version, OS/Arch, CPUs)
    - Useful for debugging and version tracking

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/ibrahmsql/gocat/cmd"
 )
@@ -25,4 +26,19 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+// showVersion displays version information
+func showVersion() {
+	fmt.Printf("GoCat %s\n", version)
+	fmt.Printf("\nBuild Information:\n")
+	fmt.Printf("  Version:     %s\n", version)
+	fmt.Printf("  Git Commit:  %s\n", gitCommit)
+	fmt.Printf("  Git Branch:  %s\n", gitBranch)
+	fmt.Printf("  Build Time:  %s\n", buildTime)
+	fmt.Printf("  Built By:    %s\n", builtBy)
+	fmt.Printf("\nRuntime Information:\n")
+	fmt.Printf("  Go Version:  %s\n", runtime.Version())
+	fmt.Printf("  OS/Arch:     %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("  CPUs:        %d\n", runtime.NumCPU())
 }
